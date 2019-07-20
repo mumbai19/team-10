2942ce77a772bf433bbb21710dcae6ed3fdd8b9f
sahliparekh08 <sahilparekh08@gmail.com>
2019-07-20 19:29:52 +0530

    sahil1

--- trishul/trishul/urls.py
+++ trishul/trishul/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .views import home_page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_page),
+
 ]
