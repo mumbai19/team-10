34770ce239fa5746a5b0e5253fcc7e50c1739918
Sladyn <gunnerforlife00@gmail.com>
2019-07-21 00:09:55 +0530

    Working code

--- AndroidApplication/app/src/main/java/com/example/android/fundraiser/Payments.java
+++ AndroidApplication/app/src/main/java/com/example/android/fundraiser/Payments.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Payments extends AppCompatActivity {
 
@@ -17,10 +19,9 @@ public class Payments extends AppCompatActivity {
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_LONG).show();
-//                String msg = "This message comes from PassingDataSourceActivity's first button";
-//                EditText edtEditText = (EditText)findViewById(R.id.PaymentAmount);
-//                String amount = edtEditText.getText().toString();
+                EditText edtEditText = (EditText)findViewById(R.id.PaymentAmount);
+                String amount = edtEditText.getText().toString();
+                Toast.makeText(getApplicationContext(), amount, Toast.LENGTH_LONG).show();
                 Intent intent =new Intent(Payments.this,PaymentGateway.class);
                 Payments.this.startActivity(intent);
 //                OpenGateway(msg);
