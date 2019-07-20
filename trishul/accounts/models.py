b9adf14fad88edb4c2f40e8d57f523d8ad0f8a7c
apekshasansare <apekshasansare305@gmail.com>
2019-07-20 20:10:09 +0530

    PhoneNumber error solved

--- trishul/accounts/models.py
+++ trishul/accounts/models.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 from django.template.loader import get_template
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.phonenumber_field.modelfields import PhoneNumberField
 
-class GuestDetails(models.Model):
+
+class UserProfile(models.Model):
     email =models.EmailField()
     mobile_number  = PhoneNumberField(default='9999999999')
     first_name     = models.CharField(max_length=255, default='John')
@@ -24,13 +26,13 @@ class User(AbstractBaseUser):
 	objects = UserManager()
 
 	def __str__(self):
-        return self.email
+		return self.email
+	
+	def has_perm(self, perm, obj=None):
+		return True
 
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
+	def has_module_perms(self, app_label):
+		return True
 
 
 
