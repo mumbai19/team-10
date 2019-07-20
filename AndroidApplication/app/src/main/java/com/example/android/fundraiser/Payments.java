de509b7b6c9e50ad8a2826f67f7cd49347f7c30a
jpg-130 <jigsgandhi97@gmail.com>
2019-07-20 23:39:53 +0530

    Added Payments Activity

--- AndroidApplication/app/src/main/java/com/example/android/fundraiser/Payments.java
+++ AndroidApplication/app/src/main/java/com/example/android/fundraiser/Payments.java
@@ -1,30 +1,28 @@
 package com.example.android.fundraiser;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+public class Payments extends AppCompatActivity {
 
-public class Payment extends AppCompatActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.payment);
+        setContentView(R.layout.activity_payments);
         Button pay = (Button)findViewById(R.id.Pay);
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_LONG).show();
 //                String msg = "This message comes from PassingDataSourceActivity's first button";
-                EditText edtEditText = (EditText)findViewById(R.id.PaymentAmount);
-                String amount = edtEditText.getText().toString();
-                Intent intent =new Intent(Payment.this,PaymentGateway.class);
-                Payment.this.startActivity(intent);
+//                EditText edtEditText = (EditText)findViewById(R.id.PaymentAmount);
+//                String amount = edtEditText.getText().toString();
+                Intent intent =new Intent(Payments.this,PaymentGateway.class);
+                Payments.this.startActivity(intent);
 //                OpenGateway(msg);
             }
         });
