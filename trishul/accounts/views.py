b834f95443d2a3d677e600c564e0297103ec1973
apekshasansare <apekshasansare305@gmail.com>
2019-07-20 19:01:17 +0530

    Login and Registration Backend

--- trishul/accounts/views.py
+++ trishul/accounts/views.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+import requests
+from django.urls import reverse
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import CreateView, FormView, DetailView, UpdateView
+from django.utils.http import is_safe_url
+from django.contrib.auth import authenticate, login, get_user_model
+from .forms import LoginForm, RegisterForm, GuestForm, UserDetailChangeForm
+from .models import GuestDetails
+from .signals import user_logged_in
+from ecommerce.mixins import NextUrlMixin, RequestFormAttachMixin
 
-from django.shortcuts import render
+class LoginView(NextUrlMixin, RequestFormAttachMixin, FormView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    sucess_url = '/'
+    default_next = '/'
 
-# Create your views here.
+    def form_valid(self, form):
+        next_path = self.get_next_url()
+        return redirect(next_path)
+
+
+class LoginView(NextUrlMixin, RequestFormAttachMixin, FormView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    sucess_url = '/'
+    default_next = '/'
+
+    def form_valid(self, form):
+        next_path = self.get_next_url()
+        return redirect(next_path)
+
+
